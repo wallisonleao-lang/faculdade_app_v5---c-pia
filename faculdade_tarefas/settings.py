0047a442ceb6e86c18ec537b07074df0f7f010f2
Wallison Pereira Nobre de São Leão <wallison@MacBook-Air-de-Wallison.local>
2026-02-03 19:56:16 -0300

    secure debug and allowed hosts via env

--- faculdade_tarefas/settings.py
+++ faculdade_tarefas/settings.py
@@ -24,9 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+xor$&ht)$s+#e(&qy(6^!4$7iowb)3d^+5oqcfo@a4x*qy)9g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "0") == "1"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = (
+    [host.strip() for host in os.getenv("ALLOWED_HOSTS", "").split(",") if host.strip()]
+    or ["faculdade-app-v5-c-pia.onrender.com"]
+)
 
 
 # Application definition
@@ -49,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,7 +128,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 SITE_ID = 1
 
